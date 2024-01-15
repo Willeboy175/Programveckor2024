dc343dcca8e878005fa2246dd57ae8a3798057dc
LeoSnattus <robin06.sima@gmail.com>
2024-01-15 14:42:19 +0100

    ändrade så att firing point är längre upp

--- Assets/Scripts/Shooting/Shoot.cs
+++ Assets/Scripts/Shooting/Shoot.cs
@@ -38,7 +38,7 @@ public class Shoot : MonoBehaviour
             bulletRb.AddForce(shootDirection * bulletForce, ForceMode2D.Impulse);
         }
 
-        bulletRb.gravityScale = 0f;
+        bulletRb.gravityScale = 1f;
 
         Destroy(bullet, 3f);
     }
