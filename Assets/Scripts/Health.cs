40832f14054153756f08139fc33be319b796f21e
OsamaBanana <120008028+OsamaBanana@users.noreply.github.com>
2024-01-16 15:50:42 +0100

    Hälsa och sprite för soptunnan

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public int maxHealth = 100; //sätter hälsan av gubben
+    public GameObject HealthBar, HealthBar2, HealthBar3; //Ska visa spelaren hur mycket hälsa man har kvar
+    public int maxHealth = 3; //sätter hälsan av gubben
     private int currentHealth; // Är en check på hur mycket hälsa gubben har kvar
 
     void Start()
@@ -21,7 +22,27 @@ public class Health : MonoBehaviour
             Die();
         }
     }
-
+    void Update()
+    {
+        if (currentHealth == 2)
+        {
+            HealthBar3.SetActive(false);
+        }
+        if (currentHealth == 1)
+        {
+            HealthBar2.SetActive(false);
+        }
+        if (currentHealth <= 0)
+        {
+            HealthBar.SetActive(false);
+        }
+        else if (currentHealth == maxHealth)
+        {
+            HealthBar.SetActive(true);
+            HealthBar2.SetActive(true);
+            HealthBar3.SetActive(true);
+        }
+    }
     // När gubben dör tas objectet bort
     void Die()
     {
