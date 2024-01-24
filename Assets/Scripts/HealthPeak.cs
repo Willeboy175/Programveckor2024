5267d706a4569bfcbf6d6768ab1ede726ab335a4
OsamaBanana <120008028+OsamaBanana@users.noreply.github.com>
2024-01-24 10:54:54 +0100

    HP Pack
    Jaja den funkar nu

--- Assets/Scripts/HealthPeak.cs
+++ Assets/Scripts/HealthPeak.cs
@@ -6,34 +6,27 @@ public class HealthPeak : MonoBehaviour
 {
     public float interactRadius = 0.2f; // Radius within which the player can interact with the trash can
     public LayerMask playerLayer; // Layer mask to filter players for interaction
-    public GameObject interactionText; //Text will appear when the player is close to the trashcan
-
+    public GameObject HealthPack;
     MovementScript playerMovement;
     Health healthValues;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Collider2D colliders = Physics2D.OverlapCircle(transform.position, interactRadius, playerLayer);
         if (colliders != null && Health.currentHealth < Health.maxHealth)
         {
-            interactionText.SetActive(true);
             playerMovement = colliders.GetComponent<MovementScript>();
             Debug.Log("Using Healhtpack");
             Activ8HealthPack(playerMovement, healthValues);
-        }
-        else
-        {
-            interactionText.SetActive(false);
         }
     }
     void Activ8HealthPack(MovementScript playerMovement, Health healthValues)
     {
         Health.currentHealth += 1;
+        Deactiv8HealthPack();
+    }
+    void Deactiv8HealthPack() 
+    {
+        Destroy(HealthPack);
     }
 }
